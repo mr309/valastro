5436792e0af8940b5c7579ad99d93d493010ea50
mr309 <65901568+mr309@users.noreply.github.com>
2023-09-06 21:43:45 -0400

    Update astro.config.mjs

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://mr309.github.io/valastro',
 	integrations: [mdx(), sitemap()],
 });
