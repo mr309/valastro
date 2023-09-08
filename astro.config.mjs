0cb3c481287214e55ae93cc387c1b25f5139b64a
Mat <mathew.rayman@gmail.com>
2023-09-07 22:04:53 -0400

    Changed url in astro.config.mjs to make links work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mr309.github.io',
-	base: '/valastro',
-	integrations: [mdx(), sitemap()],
+  site: "https://mr309.github.io/valastro",
+  integrations: [mdx(), sitemap()],
 });
